1624f06f0d2a85f6767189cef0dfe151d603be82
lucaass-d3v <lucas.workps@gmail.com>
2025-11-16 19:54:29 -0300

    refactor: Lógica de tokens do lexer modificada.  - Agora sua busca por tokens é de O(1) usando HashMap

--- core/src/main/java/org/klang/core/lexer/Lexer.java
+++ core/src/main/java/org/klang/core/lexer/Lexer.java
@@ -1,6 +1,7 @@
 package org.klang.core.lexer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.klang.core.errors.Diagnostic;
@@ -14,13 +15,14 @@ public class Lexer {
     int position = 0, line = 1, column = 0;
     String source;
     String filePath;
+    HashMap<String, TokenType> tokensTypeByString = new HashMap<>();
+    HashMap<Character, TokenType> tokensTypeByChar = new HashMap<>();
 
     public List<Token> tokenize() {
         StringBuilder s = new StringBuilder();
-        s.setLength(0);
 
-        // Enquanto não for o fim da string
         while (!EOF()) {
+            s.setLength(0); // Limpar builder
             char c = source.charAt(position);
             column++;
 
@@ -43,7 +45,6 @@ public class Lexer {
 
                 tokens.add(new Token(TokenType.STRING, s.toString()));
 
-                s.setLength(0);
                 continue;
             }
 
@@ -55,11 +56,10 @@ public class Lexer {
 
                 tokens.add(new Token(TokenType.CHARACTER, s.toString()));
 
-                s.setLength(0);
                 continue;
             }
 
-            if (isLetter(c)) {
+            if (Character.isLetter(c)) {
                 s.append(c);
 
                 while (hasNext()) {
@@ -71,28 +71,14 @@ public class Lexer {
 
                 String value = s.toString();
 
-                switch (value) {
-                    case "return", "if", "otherwise", "afterall", "for", "while", "break", "continue", "public",
-                            "private", "static", "protected" -> {
-                        tokens.add(new Token(TokenType.KEYWORD, value));
-
-                        break;
-                    }
-
-                    default -> {
-                        tokens.add(new Token(TokenType.IDENTIFIER, value));
-
-                        break;
-                    }
-                }
-
-                s.setLength(0); // Limpar builder
+                TokenType tokenType = tokensTypeByString.getOrDefault(value, TokenType.IDENTIFIER);
+                tokens.add(new Token(tokenType, value));
 
                 position++;
                 continue;
             }
 
-            if (isDigit(c)) {
+            if (Character.isDigit(c)) {
 
                 s.append(c);
 
@@ -107,188 +93,96 @@ public class Lexer {
 
                 tokens.add(new Token(TokenType.NUMBER, value));
 
-                s.setLength(0); // Limpar builder
                 position++;
 
                 continue;
             }
 
+            TokenType tokenType = tokensTypeByChar.getOrDefault(c, null);
+
+            if (tokenType == null) {
+                Span span = new Span(filePath, line, column, line, column + 1);
+                Diagnostic d = new Diagnostic(
+                        DiagnosticType.LEXICAL,
+                        "Caractere inesperado '" + c + "'",
+                        span).addNote(new Note("Remova esse caractere ou corrija o token!"));
+
+                throw new DiagnosticException(d);
+            }
+
             switch (c) {
-                case '@' -> {
-                    tokens.add(new Token(TokenType.AT));
-                    break;
-                }
-
-                case ',' -> {
-                    tokens.add(new Token(TokenType.COMMA));
-
-                    break;
-                }
-
                 case '=' -> {
-                    if (hasNext()) {
+                    if (getNext() == '=') {
                         tokens.add(new Token(TokenType.DOUBLEEQUAL));
                         position++;
 
                         break;
                     }
-
-                    tokens.add(new Token(TokenType.ASSIGNMENT));
+                    tokens.add(new Token(tokenType));
 
                     break;
                 }
 
                 case '+' -> {
-                    if (hasNext()) {
+                    if (getNext() == '+') {
                         tokens.add(new Token(TokenType.INCREMENT));
                         position++;
 
                         break;
                     }
-
-                    tokens.add(new Token(TokenType.PLUS));
+                    tokens.add(new Token(tokenType));
 
                     break;
                 }
 
                 case '-' -> {
-                    if (hasNext()) {
+                    if (getNext() == '-') {
                         tokens.add(new Token(TokenType.DECREMENT));
                         position++;
 
                         break;
                     }
 
-                    tokens.add(new Token(TokenType.MINUS));
+                    if (getNext() == '>') {
+                        tokens.add(new Token(TokenType.ARROW));
+                        position++;
 
-                    break;
-                }
+                        break;
 
-                case '*' -> {
-                    tokens.add(new Token(TokenType.MULTIPLY));
+                    }
 
-                    break;
-                }
-
-                case '/' -> {
-                    tokens.add(new Token(TokenType.DIVISION));
-
-                    break;
-                }
-
-                case '%' -> {
-                    tokens.add(new Token(TokenType.REMAINDER));
-
-                    break;
-                }
-
-                case ';' -> {
-                    tokens.add(new Token(TokenType.SEMICOLON));
-
-                    break;
-                }
-
-                case '(' -> {
-                    tokens.add(new Token(TokenType.LPAREN));
-
-                    break;
-                }
-
-                case ')' -> {
-                    tokens.add(new Token(TokenType.RPAREN));
-
-                    break;
-                }
-
-                case '{' -> {
-                    tokens.add(new Token(TokenType.LBRACE));
-
-                    break;
-                }
-
-                case '}' -> {
-                    tokens.add(new Token(TokenType.RBRACE));
-
-                    break;
-                }
-
-                case '[' -> {
-                    tokens.add(new Token(TokenType.LBRACKET));
-
-                    break;
-                }
-
-                case ']' -> {
-                    tokens.add(new Token(TokenType.RBRACKET));
+                    tokens.add(new Token(tokenType));
 
                     break;
                 }
 
                 case '>' -> {
-                    if (hasNext()) {
+                    if (getNext() == '=') {
                         tokens.add(new Token(TokenType.GTE));
                         position++;
 
                         break;
                     }
 
-                    tokens.add(new Token(TokenType.GT));
+                    tokens.add(new Token(tokenType));
                     break;
                 }
 
                 case '<' -> {
-                    if (hasNext()) {
+                    if (getNext() == '=') {
                         tokens.add(new Token(TokenType.LTE));
                         position++;
 
                         break;
                     }
-
-                    tokens.add(new Token(TokenType.LT));
-
-                    break;
-                }
-
-                case '.' -> {
-                    tokens.add(new Token(TokenType.DOT));
+                    tokens.add(new Token(tokenType));
 
                     break;
-                }
-
-                case ':' -> {
-                    tokens.add(new Token(TokenType.COLON));
-
-                    break;
-                }
-
-                case '!' -> {
-                    tokens.add(new Token(TokenType.BANG));
-
-                    break;
-                }
-                /*
-                 * --- base ---
-                 * 
-                 * case '' -> {
-                 * tokens.add(new Token(TokenType.));
-                 * position++;
-                 * 
-                 * break;
-                 * }
-                 */
-
-                default -> {
-                    Span span = new Span(filePath, line, column, line, column + 1);
-                    Diagnostic d = new Diagnostic(
-                            DiagnosticType.LEXICAL,
-                            "Caractere inesperado '" + c + "'",
-                            span).addNote(new Note("Remova esse caractere ou corrija o token!"));
-
-                    throw new DiagnosticException(d);
                 }
 
             }
 
+            tokens.add(new Token(tokenType));
             position++;
         }
 
@@ -306,7 +200,13 @@ public class Lexer {
             if (c == '\\') {
                 position++;
                 if (position >= source.length()) {
-                    throw new RuntimeException("String não fechada: fim de arquivo inesperado");
+                    Span span = new Span(filePath, line, column, line, column + 1);
+                    Diagnostic d = new Diagnostic(
+                            DiagnosticType.LEXICAL,
+                            "String não fechada: fim de arquivo inesperado",
+                            span).addNote(new Note("Esperado \'"));
+
+                    throw new DiagnosticException(d);
                 }
                 char escaped = source.charAt(position);
                 s.append('\\').append(escaped);
@@ -317,7 +217,13 @@ public class Lexer {
             position++;
 
             if (position >= source.length()) {
-                throw new RuntimeException("String não fechada: fim de arquivo inesperado");
+                Span span = new Span(filePath, line, column, line, column + 1);
+                Diagnostic d = new Diagnostic(
+                        DiagnosticType.LEXICAL,
+                        "String não fechada: fim de arquivo inesperado",
+                        span).addNote(new Note("Esperado \'"));
+
+                throw new DiagnosticException(d);
             }
 
             c = source.charAt(position);
@@ -336,7 +242,13 @@ public class Lexer {
             if (c == '\\') {
                 position++;
                 if (position >= source.length()) {
-                    throw new RuntimeException("String não fechada: fim de arquivo inesperado");
+                    Span span = new Span(filePath, line, column, line, column + 1);
+                    Diagnostic d = new Diagnostic(
+                            DiagnosticType.LEXICAL,
+                            "String não fechada: fim de arquivo inesperado",
+                            span).addNote(new Note("Esperado \""));
+
+                    throw new DiagnosticException(d);
                 }
                 char escaped = source.charAt(position);
                 s.append('\\').append(escaped);
@@ -347,7 +259,13 @@ public class Lexer {
             position++;
 
             if (position >= source.length()) {
-                throw new RuntimeException("String não fechada: fim de arquivo inesperado");
+                Span span = new Span(filePath, line, column, line, column + 1);
+                Diagnostic d = new Diagnostic(
+                        DiagnosticType.LEXICAL,
+                        "String não fechada: fim de arquivo inesperado",
+                        span).addNote(new Note("Esperado \""));
+
+                throw new DiagnosticException(d);
             }
 
             c = source.charAt(position);
@@ -356,85 +274,80 @@ public class Lexer {
         return s.toString();
     }
 
-    private boolean isLetter(char c) {
-        boolean is = false;
-        char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
-        for (char letter : letters) {
-            if (c == letter) {
-                is = true;
-                break;
-            }
-        }
-
-        // System.out.println("No is letter");
-
-        return is;
-    }
-
-    private boolean isDigit(char c) {
-        boolean is = false;
-        char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-        for (char digit : digits) {
-            if (c == digit) {
-                is = true;
-                break;
-            }
-        }
-
-        return is;
-    }
-
     private boolean isNewLine(char c) {
         return c == '\n';
 
     }
 
     private boolean hasNext() {
-        // Verifica se é o ultimo
         if (EOF()) {
             return false;
         }
 
         char c = source.charAt(position + 1);
-
-        return isLetter(c) || isDigit(c) || isLogical(c);
+        return Character.isLetter(c) || Character.isDigit(c) || isLogicalChar(c);
     }
 
-    private boolean isLogical(char c) {
+    private char getNext() {
+        if (EOF()) {
+            return '\0';
+        }
 
+        return source.charAt(position + 1);
+    }
+
+    private boolean isLogicalChar(char c) {
         return c == '=' || c == '-' || c == '+' || c == '>';
     }
 
-    /*
-     * 
-     * private boolean isSeparator(char c) {
-     * return c == ',';
-     * }
-     * 
-     * 
-     * private boolean isDelimiter(char c) {
-     * return c == '(' || c == ')' || c == '\'' || c == '\"';
-     * }
-     */
-
     private boolean EOF() {
-        if (position >= source.length()) {
-            return true;
-        }
+        return position >= source.length();
+    }
 
-        return false;
+    private void initialzerhashMapTokensTypes() {
+        tokensTypeByString.put("return", TokenType.KEYWORD);
+        tokensTypeByString.put("if", TokenType.KEYWORD);
+        tokensTypeByString.put("otherwise", TokenType.KEYWORD);
+        tokensTypeByString.put("afterall", TokenType.KEYWORD);
+        tokensTypeByString.put("for", TokenType.KEYWORD);
+        tokensTypeByString.put("while", TokenType.KEYWORD);
+        tokensTypeByString.put("break", TokenType.KEYWORD);
+        tokensTypeByString.put("continue", TokenType.KEYWORD);
+        tokensTypeByString.put("public", TokenType.KEYWORD);
+        tokensTypeByString.put("private", TokenType.KEYWORD);
+        tokensTypeByString.put("static", TokenType.KEYWORD);
+        tokensTypeByString.put("protected", TokenType.KEYWORD);
+        tokensTypeByChar.put('@', TokenType.AT);
+        tokensTypeByChar.put(',', TokenType.COMMA);
+        tokensTypeByChar.put('+', TokenType.PLUS);
+        tokensTypeByChar.put('=', TokenType.ASSIGNMENT);
+        tokensTypeByChar.put('*', TokenType.MULTIPLY);
+        tokensTypeByChar.put('/', TokenType.DIVISION);
+        tokensTypeByChar.put('-', TokenType.MINUS);
+        tokensTypeByChar.put('%', TokenType.REMAINDER);
+        tokensTypeByChar.put(';', TokenType.SEMICOLON);
+        tokensTypeByChar.put('(', TokenType.LPAREN);
+        tokensTypeByChar.put(')', TokenType.RPAREN);
+        tokensTypeByChar.put('{', TokenType.LBRACE);
+        tokensTypeByChar.put('}', TokenType.RBRACE);
+        tokensTypeByChar.put('>', TokenType.GT);
+        tokensTypeByChar.put('<', TokenType.LT);
+        tokensTypeByChar.put('.', TokenType.DOT);
+        tokensTypeByChar.put(':', TokenType.COLON);
+        tokensTypeByChar.put('!', TokenType.BANG);
+        tokensTypeByChar.put('[', TokenType.LBRACKET);
+        tokensTypeByChar.put(']', TokenType.RBRACKET);
+
     }
 
     public Lexer(String source, String filePath) {
         this.source = source;
         this.filePath = filePath;
+
+        initialzerhashMapTokensTypes();
     }
 
-    public void testarTokenize() {
+    public void testTokenize() {
         this.source = "integer x = 10;";
 
         tokenize();
